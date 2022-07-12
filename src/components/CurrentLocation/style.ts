9173a8b2e8d77ff607dd7c3eb8db8b4689ae3fe4
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-07-12 18:31:21 -0300

    feat: Add CurrentLocation component. Button that sets the currentLocation of the browser when clicked

---
+++ src/components/CurrentLocation/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    svg {
+        font-size: 1.5rem;
+
+        &:hover{
+            cursor: pointer;
+            filter: brightness(0.8);
+        }
+    }
+`
