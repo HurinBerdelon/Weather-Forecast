48290fa442c3b5301b2ed252ade62c1cefdba4b0
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-07-12 17:31:09 -0300

    feat: Add getCurrentLocation button in Input component

--- src/components/InputLocation/index.tsx
+++ src/components/InputLocation/index.tsx
@@ -17,9 +17,25 @@ interface CoordinatesProps {
 
 export function InputLocation(): JSX.Element {
 
+    // States to store the place object returned by google-place-autocomplete
+    // and the coordinates of latitude and longitude
     const [place, setPlace] = useState<PlaceProps>()
     const [coordinates, setCoordinates] = useState<CoordinatesProps>()
 
+    // function that gets the current position of the user, based on their browser geolocation
+    function getCurrentPosition() {
+        navigator.geolocation.getCurrentPosition((position) => {
+
+            setCoordinates({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            })
+        })
+    }
+
+    // Everytime the place changes, call the function that returns the coordinates of that place
+    // only executes if the place exists, because when the page is mounted, 
+    // place is undefined, which means it has no property label
     useEffect(() => {
         if (place) {
             geocodeByAddress(place.label)
@@ -29,6 +45,7 @@ export function InputLocation(): JSX.Element {
         }
     }, [place])
 
+    // render the component of google-place-autocomplete and a button to get the current location
     return (
         <Container>
             <GooglePlacesAutocomplete
@@ -36,6 +53,11 @@ export function InputLocation(): JSX.Element {
                 debounce={500}
                 selectProps={{ place, onChange: setPlace }}
             />
+            <button
+                onClick={getCurrentPosition}
+            >
+                Posição atual
+            </button>
         </Container>
     )
 }
