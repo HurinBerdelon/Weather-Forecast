69472ba891475cfa952da92f7611216b1dd411d7
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-07-13 17:57:43 -0300

    feat: Add Header component, with the name of the app

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./style";
+
+export function Header(): JSX.Element {
+
+    return (
+        <Container>
+            <h1>Weather Forecast APP</h1>
+        </Container>
+    )
+}
---
+++ src/components/Header/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    color: var(--gray-900);
+    margin: 1rem;
+    text-align: center;
+`
